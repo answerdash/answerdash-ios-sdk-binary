c25b7137505648788fbb4d681eef402bc0ce682e
Harry Soehalim <harry@answerdash.com>
2016-09-01 12:04:39 -0700

    latest AnswerDashSDK.framework

---
+++ SimpleDemoObjectiveC/AnswerDashDemo/AnswerDashDemo.xcodeproj/AnswerDashSDK.framework/Headers/AnswerDashSDK.h
@@ -0,0 +1,19 @@
+//
+//  AnswerDashSDK.h
+//  AnswerDashSDK
+//
+//  Created by Channa Karunathilake on 6/17/16.
+//  Copyright © 2016 AnswerDash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnswerDashSDK.
+FOUNDATION_EXPORT double AnswerDashSDKVersionNumber;
+
+//! Project version string for AnswerDashSDK.
+FOUNDATION_EXPORT const unsigned char AnswerDashSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnswerDashSDK/PublicHeader.h>
+
+
