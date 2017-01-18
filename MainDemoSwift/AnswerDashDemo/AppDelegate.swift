3d423a3399b0517ff68fd015fc11e593c5914ff2
Harry Soehalim <harry@answerdash.com>
2017-01-18 11:00:12 -0800

    set image bg for navbar

--- MainDemoSwift/AnswerDashDemo/AppDelegate.swift
+++ MainDemoSwift/AnswerDashDemo/AppDelegate.swift
@@ -50,6 +50,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Style
 
     fileprivate func applyGlobalStyle() {
-        UINavigationBar.appearance().barStyle = .black
+        UINavigationBar.appearance().barStyle = .default
+        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "navbar-portrait"),
+                                                        for: .default)
+        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "navbar-landscape"),
+                                                        for: .compact)
     }
 }
---
+++ MainDemoSwift/AnswerDashDemo/Helpers/UINavigationBar+.swift
@@ -0,0 +1,17 @@
+//
+//  UINavigationBar+.swift
+//  AnswerDashDemo
+//
+//  Created by Harry Soehalim on 1/18/17.
+//  Copyright © 2017 AnswerDash. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        let newSize = CGSize(width: width, height: 44)
+        return newSize
+    }
+}
